e581f831cdb040f9da50a295c7f5e89aa226585d
joeyclemens <joeyclemens87@gmail.com>
2022-06-25 15:25:22 +0100

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -7,15 +7,15 @@ ENV = "PROD"
 
 
 ## keys
-# if ENV == "DEV":
-# 	from settings import keys
-# 	telegram_key = keys.telegram_key
-# 	mongodb_key = keys.mongodb_key
-#
-# elif ENV == "PROD":
-# 	import ast
-# 	telegram_key = ast.literal_eval(os.environ["telegram_key"])
-# 	mongodb_key = ast.literal_eval(os.environ["mongodb_key"])
+ if ENV == "DEV":
+ 	from settings import keys
+ 	telegram_key = keys.telegram_key
+ 	mongodb_key = keys.mongodb_key
+
+ elif ENV == "PROD":
+ 	import ast
+ 	telegram_key = ast.literal_eval(os.environ["telegram_key"])
+ 	mongodb_key = ast.literal_eval(os.environ["mongodb_key"])
 
 
 
